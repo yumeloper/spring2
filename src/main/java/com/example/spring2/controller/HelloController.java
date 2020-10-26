54d6a1b5a3c3a49732e10acf03f5cdd531e4e823
yumyum <yunyeong93naver.com@yumyumui-MacBookAir.local>
2020-10-26 18:40:00 +0900

    Initial commit

---
+++ src/main/java/com/example/spring2/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.spring2.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "helloworld";
+    }
+    @GetMapping("/greetings")
+    public String greetings(){
+
+        return "greetings";
+    }
+
+}
